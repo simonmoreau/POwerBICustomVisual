c2aa4dec62d95c2ebc2049070dc3a516f629e24a
simonmoreau <simon@bim42.com>
2018-05-31 15:02:56 +0200

    Handle invalid XML characters

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.IO;
 using System.Globalization;
 using System.Text.RegularExpressions;
+using System.Text;
 
 namespace ConvertDrawings
 {
@@ -17,64 +18,68 @@ namespace ConvertDrawings
             Console.WriteLine("Hello World!");
 
             string sourcePath = @"C:\Users\smoreau\OneDrive - Bouygues Immobilier\Bureau\PowerBI\ConvertDrawings\Sources\Architecte #14.svg";
-
+            string testPath = @"C:\Users\smoreau\OneDrive - Bouygues Immobilier\Bureau\PowerBI\ConvertDrawings\test.xml";
 
             string sourceName = System.IO.Path.GetFileNameWithoutExtension(sourcePath);
             string content = File.ReadAllText(sourcePath);
 
             //Read XMl
             XmlSerializer serializer = new XmlSerializer(typeof(Svg));
+            string encodedString = StripNonValidXMLCharacters(content);
 
-            StringReader stringReader = new StringReader(XmlConvert.EncodeName(content));
-
-            Svg svg = (Svg)serializer.Deserialize(XmlReader.Create(stringReader));
-
-            List<G> levels = svg.G.g.FirstOrDefault().g;
-
-            Shape.Svg resultingSVG = new Shape.Svg();
-
-            resultingSVG.Version = "1.1";
-            resultingSVG.Id = "Map";
-            resultingSVG.Class = "gen-by-synoptic-designer";
-            resultingSVG.Xmlns = "http://www.w3.org/2000/svg";
-            resultingSVG.Xlink = "http://www.w3.org/1999/xlink";
-            resultingSVG.ViewBox = ScaleViewPort(svg.ViewBox, 100);
-            resultingSVG.Height = "100%";
-            resultingSVG.Width = "100%";
-            resultingSVG.Space = "preserve";
-
-            List<Shape.Polygon> polygons = new List<Shape.Polygon>();
-
-            int i = 1;
-            foreach (G level in levels)
+            using (TextReader reader = new StringReader(encodedString))
             {
-                //Find all spaces
-                List<G> elements = level.g;
-                List<G> spaces = elements.Where(x => x.Class == "bs-ifcspace bs-ifcproduct").ToList();
+                Svg svg = (Svg)serializer.Deserialize(reader);
 
-                foreach (G space in spaces)
+                List<G> levels = svg.G.g.FirstOrDefault().g;
+
+                Shape.Svg resultingSVG = new Shape.Svg();
+
+                resultingSVG.Version = "1.1";
+                resultingSVG.Id = "Map";
+                resultingSVG.Class = "gen-by-synoptic-designer";
+                resultingSVG.Xmlns = "http://www.w3.org/2000/svg";
+                resultingSVG.Xlink = "http://www.w3.org/1999/xlink";
+                resultingSVG.ViewBox = ScaleViewPort(svg.ViewBox, 100);
+                resultingSVG.Height = "100%";
+                resultingSVG.Width = "100%";
+                resultingSVG.Space = "preserve";
+
+                List<Shape.Polygon> polygons = new List<Shape.Polygon>();
+
+                int i = 1;
+                foreach (G level in levels)
                 {
-                    foreach (Polygon spacePolygon in space.Polygon)
+                    //Find all spaces
+                    List<G> elements = level.g;
+                    List<G> spaces = elements.Where(x => x.Class == "bs-ifcspace bs-ifcproduct").ToList();
+
+                    foreach (G space in spaces)
                     {
-                        Shape.Polygon polygon = new Shape.Polygon();
-                        polygon.Id = space.Dataid;
-                        polygon.Title = "";
-                        polygon.Points = ScalePoints(spacePolygon.Points, 30);
+                        foreach (Polygon spacePolygon in space.Polygon)
+                        {
+                            Shape.Polygon polygon = new Shape.Polygon();
+                            polygon.Id = space.Dataid;
+                            polygon.Title = "";
+                            polygon.Points = ScalePoints(spacePolygon.Points, 30);
 
-                        polygons.Add(polygon);
-                        i++;
+                            polygons.Add(polygon);
+                            i++;
+                        }
                     }
+
+                    resultingSVG.Polygon = polygons;
+
+                    XmlSerializer ser = new XmlSerializer(typeof(Shape.Svg));
+                    string path = System.IO.Path.Combine(@"C:\Users\smoreau\OneDrive - Bouygues Immobilier\Bureau\PowerBI\ConvertDrawings\Results", sourceName + level.Name + ".svg");
+
+                    TextWriter writer = new StreamWriter(path);
+                    ser.Serialize(writer, resultingSVG);
+                    writer.Close();
                 }
+
+
             }
-
-            resultingSVG.Polygon = polygons;
-
-            XmlSerializer ser = new XmlSerializer(typeof(Shape.Svg));
-            string path = System.IO.Path.Combine(@"C:\Users\smoreau\OneDrive - Bouygues Immobilier\Bureau\PowerBI\ConvertDrawings\Results", sourceName + ".edited.svg");
-
-            TextWriter writer = new StreamWriter(path);
-            ser.Serialize(writer, resultingSVG);
-            writer.Close();
 
 
 
@@ -117,25 +122,26 @@ namespace ConvertDrawings
             return result;
         }
 
-        static void RemoveInvalidChars(string path)
+        static string StripNonValidXMLCharacters(string textIn)
         {
-            string text = File.ReadAllText(path);
+            string resultText = textIn;
 
-            Regex regex = new Regex(@"data-longname="".*?""");
-            MatchCollection matchs = regex.Matches(text);
+            Regex regex = new Regex(@"([""'])(?:(?=(\\?))\2.)*?\1");
+            MatchCollection matchs = regex.Matches(textIn);
             if (matchs.Count != 0)
             {
+
                 foreach (Match match in matchs)
                 {
-                    if (match.Value.Contains("<"))
+                    if (match.Value.Contains("<") || match.Value.Contains(">") || match.Value.Contains("&"))
                     {
-                        string newValue = match.Value.Replace("<", "").Replace("&", "");
-                        text = text.Replace(match.Value, newValue);
+                        string newValue = match.Value.Replace("<", "").Replace(">", "").Replace("&", "");
+                        resultText = resultText.Replace(match.Value, newValue);
                     }
                 }
             }
 
-            File.WriteAllText(path, text);
+            return resultText;
         }
 
     }
