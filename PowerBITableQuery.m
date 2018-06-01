f024924877d2f67405d7b952f2eb8de6169bd76a
simonmoreau <simon@bim42.com>
2018-06-01 12:31:52 +0200

    Add M Query request to create a Power BI table with URLS

---
+++ PowerBITableQuery.m
@@ -0,0 +1,24 @@
+let
+    Source = Table.FromList(  
+
+    {
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHq", Name ="Niveau -02", objectId =20336023159, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%20-02.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHr", Name ="Niveau -03", objectId =20336023155, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%20-03.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHs", Name ="Niveau 7", objectId =20336023194, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%207.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHt", Name ="Niveau -01", objectId =20336023163, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%20-01.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHu", Name ="Niveau 5", objectId =20336023186, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%205.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHv", Name ="Niveau 6", objectId =20336023190, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%206.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHw", Name ="Niveau 3", objectId =20336023178, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%203.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHx", Name ="Niveau 4", objectId =20336023182, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%204.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHy", Name ="Niveau 1", objectId =20336023170, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%201.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjHz", Name ="Niveau 2", objectId =20336023174, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%202.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKCjLx", Name ="Niveau 0", objectId =20336023166, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%200.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKE5Bw", Name ="Niveau 8", objectId =20336023198, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Niveau%208.svg"] , 
+[GlobalId ="1RMFw0RGPB4A$R3OEKEcxO", Name ="Toiture", objectId =20336023202, URL="https://raw.githubusercontent.com/simonmoreau/POwerBICustomVisual/master/Results/Architecte%20%2314Toiture.svg"]
+
+    },  
+
+    Record.FieldValues, {"GlobalId", "Name", "objectId", "URL"}
+    )  
+in
+    Source
